7dab1a4d94055a7dba6fd2714dc4485ac17dae9f
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-20 19:41:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -39,7 +39,7 @@ def alan_kay_is_known_for
       }
     }
     
-    programmer_hash[:alan_kay][:known_for][0]
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -60,7 +60,7 @@ def dennis_ritchies_language
       }
     }
 
-programmer_hash[:dennis_ritchie][:languages].tos
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
